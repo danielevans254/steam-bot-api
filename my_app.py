35b95b26dc8c9ff339df857117e9b1258e92ffa9
danielevans254 <danielevans254@yahoo.com>
2024-03-07 18:18:31 +0800

    feat: create and initialized stack barchart, need to properly display and fetch the needed data

--- my_app.py
+++ my_app.py
@@ -2,26 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
-import time
+import altair as alt
 
 LIST_OF_DEALS_URL = 'https://www.cheapshark.com/api/1.0/deals'
 
-st.title('Steam Games')
+st.title('Steam Games, CheapShark API')
 
-# TODO: Add different filters, for steamRatingText, isOnSale, (based on user input)
-# TODO: Add the query params, to be able to filter the data, based on user input
-# TODO: Based on the documentation the default is only 60 games, so we need to fetch all the games, although the api might rate limit me. after fetching and being rate limited then it shows the data 3000 might be the max based on testing it out
-# NOTE: This should be fetching 3060 games, but it only fetches 3000, there might be a limit for the table? not sure through, might be on streamlit's end
 @st.cache_data
 def fetch_list_of_deals(page_number):
     try:
-        df = pd.DataFrame()
         response = requests.get(LIST_OF_DEALS_URL, params={'pageSize': 60, 'pageNumber': page_number})
         response.raise_for_status()
         return response.json()
-
-    except requests.RequestException as e:
-        st.error(f'Returned {page_number} pages.')
+    except requests.RequestException:
         return None
 
 def load_list_of_deals():
@@ -38,23 +31,40 @@ def load_list_of_deals():
     # Only keeping the columns that are important
     columns_to_keep = ['title', 'storeID', 'gameID', 'salePrice', 'normalPrice', 'savings','metacriticScore', 'steamRatingText', 'steamRatingPercent', 'steamRatingCount','steamAppID', 'dealRating']
     df = df[columns_to_keep]
-    st.write('Games on sale:')
+    st.write('Games currently on sale:')
     st.dataframe(df)
 
     deals_chart(df)
   except requests.RequestException as e:
     st.error(f'Failed to fetch game data: {e}')
 
-def deals_chart(df):
-    print(df)
-    # Calculate price difference
-    # Create a simplified DataFrame for plotting
-    columns_to_keep = ['title', 'salePrice', 'normalPrice', 'savings']
-    df = df[columns_to_keep]
-    chart_data = pd.DataFrame(df, columns=['salePrice', 'normalPrice', 'savings'])
-    # Plot the bar chart
-    st.bar_chart(chart_data)
-    return df
+# TODO: Create the stacked chart to display correctly
+# TODO: Create a button to filter the data based on what the user want to see, for now the default i've set is 'savings',
+def deals_chart(df, page_size=60, page_number=1):
+  columns_to_keep = ['title', 'salePrice', 'normalPrice', 'savings']
+  df = df[columns_to_keep]
+
+  # Sort the dataframe by salePrice in descending order
+  df = df.sort_values('savings', ascending=True)
+
+  # FIXME: Not Working Apply pagination
+  start_index = (page_number - 1) * page_size
+  end_index = start_index + page_size
+  df = df[start_index:end_index]
+
+  chart = alt.Chart(df).mark_bar().encode(
+    x=alt.X('title', title='Product Title'),
+    y=alt.Y('value:Q', title='Price'),
+    color=alt.Color('variable:N', title='Price Type', sort=['savings', 'normalPrice', 'salePrice']),
+    tooltip=[alt.Tooltip('salePrice'), alt.Tooltip('normalPrice'), alt.Tooltip('savings')]
+  ).properties(
+    title='Deals Chart',
+  ).transform_fold(
+    ['salePrice', 'normalPrice', 'savings'],
+    as_=['variable', 'value']
+  )
+  st.altair_chart(chart, use_container_width=True)
+  return df
 
 load_list_of_deals()
 
