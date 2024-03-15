d4eb0b97628d0e8fb11216afda7fb4575c19b2a5
danielevans254 <danielevans254@yahoo.com>
2024-03-15 17:49:26 +0800

    add improvements

--- src/my_app.py
+++ src/my_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import requests
 import altair as alt
+import re
 
 LIST_OF_DEALS_URL = 'https://www.cheapshark.com/api/1.0/deals'
 LIST_OF_GAMES_URL = 'https://www.cheapshark.com/api/1.0/games'
@@ -11,6 +12,10 @@ ALERT_URL= 'https://www.cheapshark.com/api/1.0/alerts'
 
 st.title('Steam Games, CheapShark API')
 tab1, tab2, tab3, tab4 = st.tabs(["Deals", "Games", "Stores", "Alerts"])
+
+def validate_email(email):
+  pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+  return re.match(pattern, email)
 
 with tab1:
   @st.cache_data
@@ -47,119 +52,131 @@ with tab1:
   # TODO: Create the stacked chart to display correctly
   # TODO: Create a button to filter the data based on what the user want to see, for now the default i've set is 'savings',
   def deals_chart(df):
-    columns_to_keep = ['title', 'salePrice', 'normalPrice', 'savings']
-    df = df[columns_to_keep]
+      columns_to_keep = ['title', 'salePrice', 'normalPrice', 'savings']
+      df = df[columns_to_keep]
 
-    # Convert string columns to float
-    for column in ['salePrice', 'normalPrice', 'savings']:
-      df[column] = df[column].astype(float)
+      for column in ['salePrice', 'normalPrice', 'savings']:
+          df[column] = df[column].astype(float)
 
-    # Chart for normalPrice
-    df_normal = df.sort_values('normalPrice', ascending=True).head(30)
-    print(df_normal, "Normal Price Ascending")
+      df_normal = df.nlargest(30, 'normalPrice')
+      chart_normal_price = alt.Chart(df_normal).mark_bar().encode(
+          x=alt.X('normalPrice:Q', title='Normal Price', sort='descending'),
+          y=alt.Y('title', title='Product Title', sort='-x'),
+          tooltip=[alt.Tooltip('normalPrice', format='$,.2f')]
+      ).properties(
+          title='Normal Price Chart',
+      )
+      st.altair_chart(chart_normal_price, use_container_width=True)
 
-    chart_normal_price = alt.Chart(df_normal).mark_bar().encode(
-      x=alt.X('title', title='Product Title'),
-      y=alt.Y('normalPrice:Q', title='Normal Price'),
-      tooltip=[alt.Tooltip('normalPrice')]
-    ).properties(
-      title='Normal Price Chart',
-    ).configure_axis(
-      labelFontSize=16,
-      titleFontSize=16
-    ).configure_title(
-      fontSize=16
-    )
-    st.altair_chart(chart_normal_price, use_container_width=True)
+      df_sale = df.nlargest(30, 'salePrice')
+      chart_sale_price = alt.Chart(df_sale).mark_bar().encode(
+          x=alt.X('salePrice:Q', title='Sale Price', sort='descending'),
+          y=alt.Y('title', title='Product Title', sort='-x'),
+          tooltip=[alt.Tooltip('salePrice', format='$,.2f')]
+      ).properties(
+          title='Sale Price Chart',
+      )
+      st.altair_chart(chart_sale_price, use_container_width=True)
 
-    # Chart for salePrice
-    df_sale = df.sort_values('salePrice', ascending=True).head(30)
-    print(df_sale, "Sale Price Ascending")
+      df_savings = df.nlargest(30, 'savings')
+      chart_savings = alt.Chart(df_savings).mark_bar().encode(
+          x=alt.X('savings:Q', title='Savings', sort='descending'),
+          y=alt.Y('title', title='Product Title', sort='-x'),
+          tooltip=[alt.Tooltip('savings', format='$,.2f')]
+      ).properties(
+          title='Savings Chart',
+      )
+      st.altair_chart(chart_savings, use_container_width=True)
 
-    chart_sale_price = alt.Chart(df_sale).mark_bar().encode(
-      x=alt.X('title', title='Product Title'),
-      y=alt.Y('salePrice:Q', title='Sale Price'),
-      tooltip=[alt.Tooltip('salePrice')]
-    ).properties(
-      title='Sale Price Chart',
-    ).configure_axis(
-      labelFontSize=16,
-      titleFontSize=16
-    ).configure_title(
-      fontSize=16
-    )
-
-    st.altair_chart(chart_sale_price, use_container_width=True)
-
-    # Chart for savings
-    df_savings = df.sort_values('savings', ascending=True).head(30)[['savings', 'title']]
-    print(df_savings, "Savings Price Ascending")
-
-    chart_savings = alt.Chart(df_savings).mark_bar().encode(
-      x=alt.X('title', title='Product Title'),
-      y=alt.Y('savings:Q', title='Savings'),
-      tooltip=[alt.Tooltip('savings')]
-    ).properties(
-      title='Savings Chart',
-    ).configure_axis(
-      labelFontSize=16,
-      titleFontSize=16
-    ).configure_title(
-      fontSize=16
-    )
-    st.altair_chart(chart_savings, use_container_width=True)
-
-    return df
+      return df
 
   load_list_of_deals()
 
+
 with tab2:
-  def games_list(title, steamAppID):
-    try:
+  def games_list(external, steamAppID):
       st.write('Search for a game:')
-      filter_by = st.selectbox("Filter by:", ['title', 'steamAppID'])
+      col1, col2 = st.columns(2)
+      filter_by = col1.selectbox("Filter by:", ['title', 'steamAppID'])
 
       if filter_by == 'title':
-        title = st.text_input("Enter a game title:", value=steamAppID, key='steamAppID')
+          external = col2.text_input("Enter a game title:", value=steamAppID, key='steamAppID')
       else:
-        steamAppID = st.text_input("Enter a Steam App ID:", value=title, key='title')
+          steamAppID = col2.text_input("Enter a Steam App ID:", value=external, key='title')
 
-      response = requests.get(LIST_OF_GAMES_URL, params={filter_by: title})
-      response.raise_for_status()
-      games = response.json()
-      if not games:
-        st.write(f'No games found "{title}"')
-        st.write('Search for a game.')
-      else:
-        st.write('List of Games:')
-        st.dataframe(games)
-    except requests.RequestException as e:
-      st.error(f'Failed to fetch game data: {e}')
+      if st.button('Search'):
+          try:
+              response = requests.get(LIST_OF_GAMES_URL, params={filter_by: external})
+              response.raise_for_status()
+              games = response.json()
+              if not games:
+                if filter_by == 'title':
+                  st.markdown(f'No games found by **Title**: "{external}".')
+                else:
+                  st.markdown(f'No games found by **Steam App ID**: "{steamAppID}".')
+              else:
+                  st.write('List of Games:')
+                  st.dataframe(games)
+          except requests.RequestException as e:
+              st.error(f'Failed to fetch game data: {e}')
 
   games_list("", "")
 
 with tab3:
   def stores_list():
-    try:
-      response = requests.get(LIST_OF_STORES_URL)
-      response.raise_for_status()
-      stores = response.json()
-      if not stores:
-        st.write('No stores found.')
-      else:
-        st.write('List of Stores:')
-        st.dataframe(stores)
-    except requests.RequestException as e:
-      st.error(f'Failed to fetch store data: {e}')
+      try:
+          response = requests.get(LIST_OF_STORES_URL)
+          response.raise_for_status()
+          stores = response.json()
+
+          col1, col2 = st.columns(2)
+          search_field = col1.selectbox("Search by:", ['storeID', 'storeName'])
+          search_value = col2.text_input("Enter search value:")
+
+          if search_value:
+              filtered_stores = [store for store in stores if store.get(search_field) == search_value]
+              if filtered_stores:
+                  st.write('Filtered Stores:')
+                  st.dataframe(filtered_stores)
+              else:
+                  st.write('No stores found for the given search criteria.')
+
+          if not stores:
+              st.write('No stores found.')
+
+          col1, col2 = st.columns(2)
+          show_all_stores = col1.button('Show All Stores')
+
+          if show_all_stores:
+              hide_all_stores = col2.button('Hide All Stores')
+              st.write('List of all Stores:')
+              st.dataframe(stores)
+              if hide_all_stores:
+                  st.write('')  # Clear the dataframe when the Hide button is clicked
+
+      except requests.RequestException as e:
+          st.error(f'Failed to fetch store data: {e}')
 
   stores_list()
 
 with tab4:
   def edit_alerts():
-    action = st.selectbox('Select Action', ['set', 'delete'])
-    email = st.text_input('Enter User Email')
-    gameId = st.text_input('Enter Game ID')
-    price = st.text_input('Enter Price')
+    col1, col2 = st.columns(2)
+    action = col1.selectbox('Select Action', ['set', 'delete'])
+    email = col2.text_input('Enter User Email', 'example@example.com')
+
+    if not validate_email(email):
+      st.warning('Invalid email format. Please enter a valid email address.')
+
+    col1, col2 = st.columns([3,2])
+    gameId = col1.text_input('Enter Game ID')
+    price = col2.text_input('Enter Price')
+
+    if not gameId:
+          st.warning('Game ID is required.')
+    if not price:
+          st.warning('Price is required.')
+
     price = float(price) if price else None
 
     if st.button('Send Alert'):
@@ -167,30 +184,34 @@ with tab4:
         if action == 'create':
           response = requests.get(ALERT_URL, params={'action': action, 'email': email, 'gameId': gameId, 'price': price})
           response.raise_for_status()
-          st.write(f"Email sent to {email} for action: {action}")
+          st.success(f"Email sent to {email} for action: {action}")
         elif action == 'delete':
           response = requests.get(ALERT_URL, params={'action': action, 'email': email, 'gameId': gameId})
           response.raise_for_status()
-          st.write(f"Email sent to {email} for action: {action}")
+          st.success(f"Email sent to {email} for action: {action}")
         else:
-          st.write("Invalid action. Only 'set' and 'delete' actions are supported.")
+          st.error("Invalid action. Only 'set' and 'delete' actions are supported.")
       except requests.RequestException as e:
         st.error(f'Failed to fetch alert data: {e}')
 
   def manage_alerts():
-    action = st.selectbox('Select Action', ['manage'])
-    email = st.text_input('Enter Email')
+      col1, col2 = st.columns(2)
+      action = col1.selectbox('Select Action', ['manage'])
+      email = col2.text_input('Enter Users Email', 'example@example.com')
 
-    if st.button('Check Alert'):
-      try:
-        if action == 'manage':
-          response = requests.get(ALERT_URL, params={'action': action, 'email': email})
-          response.raise_for_status()
-          st.write(f"Email sent to {email} for action: {action}")
-        else:
-          st.write("Invalid action. Only 'manage' action is supported.")
-      except requests.RequestException as e:
-        st.error(f'Failed to fetch alert data: {e}')
+      if not validate_email(email):
+        st.warning('Invalid email format. Please enter a valid email address.')
+
+      if st.button('Check Alert'):
+        try:
+          if action == 'manage':
+            response = requests.get(ALERT_URL, params={'action': action, 'email': email})
+            response.raise_for_status()
+            st.success(f"Email sent to {email} for action: {action}")
+          else:
+            st.error("Invalid action. Only 'manage' action is supported.")
+        except requests.RequestException as e:
+          st.error(f'Failed to fetch alert data: {e}')
 
   edit_alerts()
   manage_alerts()
