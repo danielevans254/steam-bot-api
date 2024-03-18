4448d228ddc6f7fcc5cfdcc839ade30da7fadceb
danielevans254 <danielevans254@yahoo.com>
2024-03-18 14:07:25 +0800

    add

--- src/chat/components/sidebar.py
+++ src/chat/components/sidebar.py
@@ -1,32 +1,45 @@
 import streamlit as st
-
+from styles.messages.styles import user_message_style, assistant_message_style
 # FIXME: The kingparser already works as expected yet the selected option from this doesn't change the model, you can change it manually inside the king_parser file, this might have some caching issues, but it's not clear why it's not changing
-def select_model():
-  with st.sidebar:
-    st.title("Chatbot ☕️")
-    add_selectbox = st.selectbox(
-      "What model would you like to use?",
-      ("ChatGPT 3.5 Turbo", "Ollama 2 Chat", "Mistral", "Test")
-    )
-    st.button("New Chat")
 
-    # TODO: Make this reusable and add the ability to change the model
-    if add_selectbox == "Test":
+def new_chat():
+  if st.button("New chat history"):
+    st.session_state["chat_history"] = []
+    st.session_state["chat_history"].append({"role": "assistant", "content": "Welcome new chat!"})
+    return st.session_state["chat_history"]
+
+def model_select():
+  add_selectbox = st.selectbox(
+    "What model would you like to use?",
+    ("ChatGPT 3.5 Turbo", "Ollama 2 Chat", "Mistral", "Test")
+  )
+  return add_selectbox
+
+exported_model_value_select = model_select()
+
+def parse_selected_model():
+    if exported_model_value_select == "Test":
       st.write("You have selected Test")
       return "test"
-    if add_selectbox == "ChatGPT 3.5 Turbo":
+    if exported_model_value_select == "ChatGPT 3.5 Turbo":
       st.write("You have selected ChatGPT 3.5 Turbo")
       return "openai"
-    if add_selectbox == "Ollama 2 Chat":
+    if exported_model_value_select == "Ollama 2 Chat":
       st.write("You have selected Ollama 2 Chat")
-      return "ollama"
-    if add_selectbox == "Mistral":
+      return "llama2_chat"
+    if exported_model_value_select == "Mistral":
       st.write("You have selected Mistral")
       return "mistral"
 
+exported_add_selectbox_value = parse_selected_model()
+
+def side_bar():
+  with st.sidebar:
+    st.subheader("Chatbot ☕️")
+    st.sidebar.subheader('Chat History')
+    model_select()
+    new_chat()
     st.radio(
       "Choose a shipping method",
       ("Standard (5-15 days)", "Express (2-5 days)")
     )
-
-exported_select_model = select_model()
--- src/chat/models/king_parser.py
+++ src/chat/models/king_parser.py
@@ -1,22 +1,24 @@
 from models.openai_model import openAI_response
 from models.test_model import test_response
-from components.sidebar import exported_select_model
+from models.ollama_2_model import ollama_response
+from models.mistral_model import mistral_response
+from components.sidebar import exported_add_selectbox_value
 
 # TODO: This should be taken from the sidebar component for the select box, but for now, we'll hardcode it.
-exported_select_model = "mistral"
+exported_add_selectbox_value = "llama2_chat"
 def king_parser(user_input):
-  print(exported_select_model, "exported_select_model")
-  if exported_select_model == "openai":
+  print(exported_add_selectbox_value, "exported_select_model")
+  if exported_add_selectbox_value == "openai":
     print("openai")
     return openAI_response(user_input)
-  elif exported_select_model == "test":
+  elif exported_add_selectbox_value == "test":
     print("test")
     return test_response(user_input)
-  elif exported_select_model == "ollama":
-    print("ollama")
-    return "ollama"
-  elif exported_select_model == "mistral":
+  elif exported_add_selectbox_value == "llama2_chat":
+    print("llama2_chat")
+    return ollama_response(user_input)
+  elif exported_add_selectbox_value == "mistral":
     print("mistral")
-    return "mistral"
+    return mistral_response(user_input)
   else:
-    return f"The {exported_select_model} model is not supported yet.😞"
+    return f"The {exported_add_selectbox_value} model is not supported yet.😞"
