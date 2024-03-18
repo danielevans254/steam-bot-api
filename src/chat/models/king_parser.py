d58733fcda7ee7b1745ff74ed6557d3abb9629bd
danielevans254 <danielevans254@yahoo.com>
2024-03-18 18:48:49 +0800

    feat: add nous hermes model

--- src/chat/models/king_parser.py
+++ src/chat/models/king_parser.py
@@ -1,7 +1,8 @@
 from models.openai_model import openAI_response
 from models.test_model import test_response
-from models.ollama_2_model import ollama_response
+from models.ollama_2_model import llama2_chat_response
 from models.mistral_model import mistral_response
+from models.nous_hermes2 import nous_hermes2_response
 from components.sidebar import exported_add_selectbox_value
 
 # TODO: This should be taken from the sidebar component for the select box, but for now, we'll hardcode it.
@@ -16,9 +17,11 @@ def king_parser(user_input):
     return test_response(user_input)
   elif exported_add_selectbox_value == "llama2_chat":
     print("llama2_chat")
-    return ollama_response(user_input)
+    return llama2_chat_response(user_input)
   elif exported_add_selectbox_value == "mistral":
     print("mistral")
     return mistral_response(user_input)
+  elif exported_add_selectbox_value == "nous_hermes2":
+    return nous_hermes2_response(user_input)
   else:
     return f"The {exported_add_selectbox_value} model is not supported yet.😞"
