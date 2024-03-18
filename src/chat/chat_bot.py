b8e8790adb75b1822c268067846d9fc9ee7d4a6a
danielevans254 <danielevans254@yahoo.com>
2024-03-18 18:48:29 +0800

    feat: add nous hermes

--- src/chat/chat_bot.py
+++ src/chat/chat_bot.py
@@ -5,6 +5,8 @@ from styles.messages.styles import user_message_style, assistant_message_style
 from utils.default_messages import random_welcome_message
 from models.king_parser import king_parser
 from commands.command_list_answers import command_list_answer, is_command_list_answer
+import time
+
 def model_chat():
   st.title("Chat with our Assistant")
 
@@ -28,16 +30,27 @@ def model_chat():
     if st.button("Send the message"):
       with st.spinner("Processing request..."):
         st.session_state["chat_history"].append({"role": "user", "content": user_input})
-        st.session_state["chat_history"].append({"role": "assistant", "content": king_parser(user_input)})
+        response = king_parser(user_input)
+        st.session_state["chat_history"].append({"role": "assistant", "content": response})
     user_input = ""
   return user_input
 
+def response_generator(response):
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
 def chat_history():
   for message in st.session_state["chat_history"]:
-    if message["role"] == "user":
-      st.write(f'<div style="display: flex; justify-content: flex-start; word-wrap: break-word;"><div style="{user_message_style}">{message["content"]}</div></div>', unsafe_allow_html=True)
-    else:
-      st.write(f'<div style="display: flex; justify-content: flex-end; word-wrap: break-word;"><div style="{assistant_message_style}">{message["content"]}</div></div>', unsafe_allow_html=True)
+      with st.chat_message(message["role"]):
+              st.markdown(message["content"])
+
+# def chat_history():
+#   for message in st.session_state["chat_history"]:
+#     if message["role"] == "user":
+#       st.write(f'<div style="display: flex; justify-content: flex-start; word-wrap: break-word;"><div style="{user_message_style}">{message["content"]}</div></div>', unsafe_allow_html=True)
+#     else:
+#       st.write(f'<div style="display: flex; justify-content: flex-end; word-wrap: break-word;"><div style="{assistant_message_style}">{message["content"]}</div></div>', unsafe_allow_html=True)
 
 def clear_chat_history():
   if st.button("Clear chat history"):
