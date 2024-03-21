78c4ed1681348cfff7c80dd6d8735a10e3605181
danielevans254 <danielevans254@yahoo.com>
2024-03-21 19:56:14 +0800

    refactor: change variable name and added extra parameter

--- src/chat/models/king_parser.py
+++ src/chat/models/king_parser.py
@@ -3,25 +3,24 @@ from models.test_model import test_response
 from models.ollama_2_model import llama2_chat_response
 from models.mistral_model import mistral_response
 from models.nous_hermes2 import nous_hermes2_response
-from components.sidebar import parse_selected_model
 
 # TODO: This should be taken from the sidebar component for the select box, but for now, we'll hardcode it.
-exported_add_selectbox_value = "llama2_chat"
-def king_parser(user_input):
-  print(exported_add_selectbox_value, "exported_select_model")
-  if exported_add_selectbox_value == "openai":
+
+def king_parser(user_input, selected_model):
+  print(selected_model, "exported_select_model")
+  if selected_model == "openai":
     print("openai")
     return openAI_response(user_input)
-  elif exported_add_selectbox_value == "test":
+  elif selected_model == "test":
     print("test")
     return test_response(user_input)
-  elif exported_add_selectbox_value == "llama2_chat":
+  elif selected_model == "llama2_chat":
     print("llama2_chat")
     return llama2_chat_response(user_input)
-  elif exported_add_selectbox_value == "mistral":
+  elif selected_model == "mistral":
     print("mistral")
     return mistral_response(user_input)
-  elif exported_add_selectbox_value == "nous_hermes2":
+  elif selected_model == "nous_hermes2":
     return nous_hermes2_response(user_input)
   else:
-    return f"The {exported_add_selectbox_value} model is not supported yet.😞"
+    return f"The {selected_model} model is not supported yet.😞"
