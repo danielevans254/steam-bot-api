3750a9698866bc17ee2dd96569da10bdafc40e47
danielevans254 <danielevans254@yahoo.com>
2024-03-24 14:31:54 +0800

    feat: Add llava model to king_parser

--- src/chat/models/king_parser.py
+++ src/chat/models/king_parser.py
@@ -3,6 +3,7 @@ from models.test_model import test_response
 from models.ollama_2_model import llama2_chat_response
 from models.mistral_model import mistral_response
 from models.nous_hermes2 import nous_hermes2_response
+from models.llava_model import llava_response
 
 # TODO: This should be taken from the sidebar component for the select box, but for now, we'll hardcode it.
 
@@ -13,7 +14,7 @@ def king_parser(user_input, selected_model):
     return openAI_response(user_input)
   elif selected_model == "test":
     print("test")
-    return test_response(user_input)
+    return test_response()
   elif selected_model == "llama2_chat":
     print("llama2_chat")
     return llama2_chat_response(user_input)
@@ -22,5 +23,7 @@ def king_parser(user_input, selected_model):
     return mistral_response(user_input)
   elif selected_model == "nous_hermes2":
     return nous_hermes2_response(user_input)
+  elif selected_model == "llava":
+    return llava_response(user_input)
   else:
     return f"The {selected_model} model is not supported yet.😞"
