1f8425e45308292aa29fbd41ed2d31a92a4c19d5
danielevans254 <danielevans254@yahoo.com>
2024-03-25 11:06:10 +0800

    Remove unused imports

--- src/chat/models/llava_model.py
+++ src/chat/models/llava_model.py
@@ -1,9 +1,7 @@
-import os
-import streamlit as st
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain_community.llms import Ollama
-
+# TODO: This should include the file path of the uploaded file
 def llava_response(user_input):
   llm = Ollama(
     model="llava",
